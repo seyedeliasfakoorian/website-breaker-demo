8c47f883547941295baca7e2994edd31d7fbd1df
Elias Fakoorian <98291494+seyedeliasfakoorian@users.noreply.github.com>
2024-03-25 17:21:55 -0700

    bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import { Gallery } from "./components/gallery";
 import { Testimonials } from "./components/testimonials";
 import { Team } from "./components/Team";
 import { Contact } from "./components/contact";
-import JsonData from "./data/data.json";
+import JsonData from "./data.json";
 import SmoothScroll from "smooth-scroll";
 import "./App.css";
 import { BreakButton } from "./components/breakButton";
